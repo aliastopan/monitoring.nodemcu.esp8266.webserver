a1c307113dc63b7475796de900ec234c1585a2d9
= <taufan.augusta@protonmail.com>
2022-01-26 12:24:04 +0700

    test: soil moisture sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,55 @@
 #include <Arduino.h>
 #include <rest.api.h>
 
+#define RELAY D8
+#define RAIN_PIN A0
+float sensorValue = 0;
+
+int dryValue = 1023;
+int wetValue = 302;
+int friendlyDryValue = 0;
+int friendlyWetValue = 100;
+
 void setup()
 {
 	Serial.begin(115200);
 	while (!Serial);
 
-	API::Setup();
-	API::Start();
+	pinMode(RAIN_PIN, INPUT);
+
+	// pinMode(RELAY, OUTPUT);
+
+	// API::Setup();
+	// API::Start();
 
 }
 
-void loop() {
+void loop()
+{
+	int rawValue = analogRead(RAIN_PIN);
+	int friendlyValue = map(rawValue, dryValue, wetValue, friendlyDryValue, friendlyWetValue);
+
+	Serial.print("Raw: ");
+	Serial.print(rawValue);
+	Serial.print(" | ");
+
+	Serial.print("Friendly: ");
+  	Serial.print(friendlyValue);
+  	Serial.println("%");
+	// sensorValue = 100 - ((analogRead(RAIN_PIN)/1024) * 100);
+	// sensorValue = analogRead(RAIN_PIN);
+  	// sensorValue = map(sensorValue,550,10,0,100);
+  	// sensorValue = map(sensorValue,1024,0,0,100);
+
+	// delay(1000);
+    // Serial.print("SENSOR VALUE: ");
+    // Serial.print(sensorValue);
+    // Serial.println("%");
+
+	// delay(1000);
+	// digitalWrite(RELAY, LOW);
+	// delay(1000);
+	// digitalWrite(RELAY, HIGH);
+    // Serial.println("Bleh.");
 
 }
