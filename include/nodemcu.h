380a8ae71eacab8bce7717007ed7bafbf39f3ed3
= <taufan.augusta@protonmail.com>
2022-01-27 22:37:39 +0700

    feat: add MUX amalog reading

--- include/nodemcu.h
+++ include/nodemcu.h
@@ -1,78 +1,43 @@
 #ifndef NODEMCU
 #define NODEMCU
-
-
 #include<MUX74HC4067.h>
-
 
 #define S0 D0
 #define S1 D1
 #define S2 D2
 #define S3 D3
+#define EN D4
 #define SIG A0
 
+MUX74HC4067 mux(D4, D0, D1, D2, D3);
 
 int rawValue1 = 0;
+int rawValue2 = 0;
 
 class Board
 {
-    private:
-        static void Ch0()
-        {
-            digitalWrite(S0, LOW);
-            digitalWrite(S1, LOW);
-            digitalWrite(S2, LOW);
-            digitalWrite(S3, LOW);
-
-        }
-
-        static int Ch1()
-        {
-            digitalWrite(S0, HIGH);
-            digitalWrite(S1, LOW);
-            digitalWrite(S2, LOW);
-            digitalWrite(S3, LOW);
-
-            return digitalRead(SIG);
-
-        }
-
-        static int Ch2()
-        {
-            digitalWrite(S0, LOW);
-            digitalWrite(S1, HIGH);
-            digitalWrite(S2, LOW);
-            digitalWrite(S3, LOW);
-
-            return digitalRead(SIG);
-        }
-
     public:
         static void Setup()
         {
             Serial.begin(115200);
 
-            pinMode(SIG, INPUT);
+            mux.signalPin(SIG, INPUT, ANALOG);
 
-            pinMode(S0, OUTPUT);
-            pinMode(S1, OUTPUT);
-            pinMode(S2, OUTPUT);
-            pinMode(S3, OUTPUT);
 
         }
 
         static void Loop()
         {
-            rawValue1 = Ch1();
-            Serial.print("sensor 1: ");
-            Serial.println(rawValue1);
+            rawValue1 = mux.read(1);
+            rawValue2 = mux.read(2);
+
+            Serial.print("ch.1: ");
+            Serial.print(rawValue1);
+            Serial.print(" | ");
+            Serial.print("ch.2: ");
+            Serial.print(rawValue2);
+            Serial.print("\n");
             delay(1000);
-
-
-            // rawValue1 = analogRead(SIG);
-            // Serial.print("sensor 1: ");
-            // Serial.println(rawValue1);
-            // delay(1000);
 
 
         }
