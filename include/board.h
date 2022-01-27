4a394fbac67a8c9a4a72360e90b8043313a49958
= <taufan.augusta@protonmail.com>
2022-01-27 08:16:24 +0700

    test: alternating

--- include/board.h
+++ include/board.h
@@ -2,46 +2,83 @@
 #define BOARD
 
 #define RELAY_PIN D8
-#define SOILMOISTURE_PIN A0
+#define ANALOG_PIN A0
+
+#define ABSOLUTE_LOW D0
+#define ABSOLUTE_HIGH D1
+
+#define SENSOR_1_PIN D2
+#define SENSOR_2_PIN D3
+
+
+
 
 int rawValue = 0;
 int dryValue = 1023;
+// int wetValue = 0;
 int wetValue = 302;
 int percentageValue = 0;
 int percentageDryValue = 0;
 int percentageWetValue = 100;
 
+int rawValue1 = 0;
+int rawValue2 = 0;
+int percentageValue1 = 0;
+int percentageValue2 = 0;
+
 class Board
 {
+    private:
+    static int ReadAnalog1()
+    {
+        digitalWrite(SENSOR_1_PIN, HIGH);
+        digitalWrite(SENSOR_2_PIN, LOW);
+        return analogRead(0);
+    }
+
+    static int ReadAnalog2()
+    {
+        digitalWrite(SENSOR_1_PIN, LOW);
+        digitalWrite(SENSOR_2_PIN, HIGH);
+        return analogRead(0);
+    }
+
     public:
     static void Setup()
     {
         Serial.begin(115200);
         while (!Serial);
 
-        pinMode(RELAY_PIN, OUTPUT);
-	    pinMode(SOILMOISTURE_PIN, INPUT);
+        // pinMode(RELAY_PIN, OUTPUT);
+	    pinMode(ANALOG_PIN, INPUT);
+
+        pinMode(ABSOLUTE_LOW, OUTPUT);
+        pinMode(ABSOLUTE_HIGH, OUTPUT);
+
+	    pinMode(SENSOR_1_PIN, OUTPUT);
+	    pinMode(SENSOR_2_PIN, OUTPUT);
+
     }
 
     static void Loop()
     {
-        rawValue = analogRead(SOILMOISTURE_PIN);
-        percentageValue = map(rawValue, dryValue, wetValue, percentageDryValue, percentageWetValue);
+        digitalWrite(ABSOLUTE_LOW, LOW);
+        digitalWrite(ABSOLUTE_HIGH, HIGH);
 
+        rawValue1 = ReadAnalog1();
         delay(1000);
-        Serial.print("Raw: ");
-	    Serial.print(rawValue);
-	    Serial.print(" | ");
+        rawValue2 = ReadAnalog2();
+        delay(1000);
 
-	    Serial.print("Percentage: ");
-  	    Serial.print(percentageValue);
-  	    Serial.println("%");
+        Serial.print("sensor 1 = ");
+        Serial.print(rawValue1);
+        Serial.print(" / sensor 2 = ");
+        Serial.println(rawValue2);
+
 
     }
 
 };
-
-
 
 
 #endif
